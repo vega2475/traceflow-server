cd73ac2b18f8182af120a046bf2e426d45c90942
vega <devid367552@gmail.com>
2024-08-26 19:42:53 +0300

    implements base file structure

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.trace.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
